52a53aa408491b23d9fd63bb091977179c1f57a8
THIRSTY <dev.thirsty92@gmail.com>
2024-09-25 01:48:33 +0900

    포인트 조회 단위테스트 클래스명 변경

--- src/test/java/io/hhplus/tdd/GetPointServiceTest.java
+++ src/test/java/io/hhplus/tdd/GetPointServiceTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 
-class PointServiceTest {
+class GetPointServiceTest {
 
     @Mock
     private PointService pointService;
